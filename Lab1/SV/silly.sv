cfe314a2cdfe2ffbc8541ed654165c4bbb7e5560
axmalakar <159060500+axmalakar@users.noreply.github.com>
2024-02-21 15:15:01 -0600

    Fin

--- Lab1/SV/silly.sv
+++ Lab1/SV/silly.sv
@@ -1,5 +1,16 @@
-module silly (input  logic a, b, c, output logic y);
+module silly(input logic a,b,cin, output logic sum,cout);
    
-  assign y = ~b & ~c | a & ~b;
+  assign sum = a ^ b ^ cin;
+  assign cout = a & b | a & cin | b & cin;
    
+endmodule
+
+module rippleCarry(input logic [3:0] a,b, input logic c, output logic cout, output logic [3:0] sum);
+
+logic [2:0] int_cout;
+silly fa1(.a(a[0]), .b(b[0]), .cin(c), .sum(sum[0]),.cout(int_cout[0]));
+silly fa2(.a(a[1]), .b(b[1]), .cin(int_cout[0]), .sum(sum[1]),.cout(int_cout[1]));
+silly fa3(.a(a[2]), .b(b[2]), .cin(int_cout[1]), .sum(sum[2]),.cout(int_cout[2]));
+silly fa4(.a(a[3]), .b(b[3]), .cin(int_cout[2]), .sum(sum[3]),.cout(cout));
+
 endmodule
--- Lab1/SV/silly_tb.sv
+++ Lab1/SV/silly_tb.sv
@@ -1,61 +1,70 @@
 `timescale 1ns / 1ps
 module tb ();
 
-   logic        a;
-   logic 	b;
-   logic 	c;
-   logic 	y;
-   logic        clk;   
+   logic [3:0] 	a;
+   logic [3:0] 	b;
+   logic 		cin;
+   logic		cout;
+   logic [3:0] 	sum;
+   logic [3:0] 	sum_correct;
+
+   logic        clk;
+   logic [31:0] errors;
+   logic [31:0] vectornum;   
+
+  
    
   // instantiate device under test
-   silly dut (a, b, c, y);
+   rippleCarry dut (.a(a), .b(b), .c(cin), .sum(sum), .cout(cout));
  ////////////////////////////////////////////////////////////////////
    // 20 ns clock
    initial 
      begin	
 	clk = 1'b1;
-	forever #10 clk = ~clk;
+	forever #5 clk = ~clk;
      end
 
+	assign sum_correct = a + b + cin;
+	integer handle3;
+	integer desc3;
+	integer i;
 
-   initial
-     begin
-    
-	#0   a = 1'b0;	
-	#0   b = 1'b0;
-	#0   c = 1'b0;
+	initial 
+	begin
+	handle3 = $fopen("rca.out");
+	desc3 = handle3;
+	#15000 $finish;
+	end
 
-	#20  a = 1'b1;
-	#0   b = 1'b0;
-	#0   c = 1'b0;
+	initial 
+	begin
+	for (i=0; i < 150; i=i+1)
+	begin
+	// Put vectors before beginning of clk
+	@(posedge clk)
+	begin
+	a = $random;
+	b = $random;
+	cin = $random;
+	end
 
-	#20  a = 1'b0;
-	#0   b = 1'b1;
-	#0   c = 1'b0;
-
-	#20  a = 1'b1;
-	#0   b = 1'b1;
-	#0   c = 1'b0;
-
-	#0   a = 1'b0;	
-	#0   b = 1'b0;
-	#0   c = 1'b1;
-
-	#20  a = 1'b1;
-	#0   b = 1'b0;
-	#0   c = 1'b1;
-
-	#20  a = 1'b0;
-	#0   b = 1'b1;
-	#0   c = 1'b1;
-
-	#20  a = 1'b1;
-	#0   b = 1'b1;
-	#0   c = 1'b1;		
+	@(negedge clk)
+	begin
+		vectornum = vectornum + 1;
+		/* if(sum != sum_correct)
+			begin
+				errors = errors + 1;
+				$display("Error: %h %h || %h = %h", a,b,sum,sum_correct);
+			end 
+			*/
+		$fdisplay(desc3, "%b %b %b || %b | %b | %b", a, b ,cin, sum, sum_correct, (sum == sum_correct));
+	end
+	end // @(negedge clk)
+	//$display("%d tests completed with %d errors", vectornum, errors);
+	//#0 $finsh;
+	end
 
 
-	
-     end
-
+ 
    
 endmodule
